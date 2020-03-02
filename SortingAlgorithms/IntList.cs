3f91b7fc42a119540ae987849ce1477c894caf4a
Nico Bohn <n.bohn97@gmail.com>
2020-03-03 00:04:15 +0100

    Added BubbleSort

---
+++ SortingAlgorithms/IntList.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortingAlgorithms
+{
+    public class IntList
+    {
+        private List<int> numList { get; set; }
+        
+        public IntList(){}
+        
+        public IntList(int amount)
+        {
+            this.numList = CreateRandomNumberList(amount);
+        }
+        
+        
+        public static List<int> CreateRandomNumberList(int numAmount)
+        {
+            var numberlist = new List<int>();
+            
+            //Console.WriteLine("Enter amount of numbers");
+            
+            // Fill List
+            for (var i = 0; i < numAmount; i++)
+            {
+                numberlist.Add(i);
+            }
+            
+            // Randomize List using Fisher-Yates Shuffle
+            var rand = new Random();
+            var n = numberlist.Count;
+
+            for (var i = numberlist.Count - 1; i > 1 ; i--)
+            {
+                var random = rand.Next(i + 1);
+                var value = numberlist[random];
+                numberlist[random] = numberlist[i];
+                numberlist[i] = value;
+                
+            }
+            return numberlist;
+        }
+
+        public void PrintList()
+        {
+            foreach (var num in numList )
+            {
+                Console.Write(num);
+                Console.Write("|");
+            }
+        }
+        
+        public bool IsSorted()
+        {
+            for (var i = 0; i < numList.Count-1; i++)
+            {
+                if (numList[i] >= numList[i + 1])
+                {
+                    return false;
+                } 
+            }
+            return true;
+        }
+
+
+
+        public List<int> BubbleSort()
+        {
+            var x = numList.Count;
+            var iteration = 0;
+            for (var i = 0; i < x-1; i++)
+            {
+                for (var j = 0; j < x-i-1; j++)
+                    if (numList[j] > numList[j + 1])
+                    {
+                        var temp = numList[j];
+                        numList[j] = numList[j + 1];
+                        numList[j + 1] = temp;
+                    }
+                
+                iteration++;
+            }
+
+            Console.WriteLine($"Number of Outer Iterations: {iteration}");
+            return numList;
+        }
+
+        public List<int> BubbleSortOptimized()
+        {
+            var x = numList.Count;
+            var iteration = 0;
+            bool swap = true;
+            for (var i = 0; i < x-1; i++)
+            {
+                for (var j = 0; j < x-i-1; j++)
+                    if (numList[j] > numList[j + 1])
+                    {
+                        var temp = numList[j];
+                        numList[j] = numList[j + 1];
+                        numList[j + 1] = temp;
+                    }
+                
+                iteration++;
+            }
+
+            Console.WriteLine($"Number of Outer Iterations: {iteration}");
+            return numList;
+            }
+        }
+        
+        
+        
+        
+        
+        
+    }
+}
--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -1,12 +1,205 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+using System.Threading;
 
 namespace SortingAlgorithms
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+            
+            // Assign Name and Create Txt File 
+            var path = AssignPath();
+            var stream = CreateFile(path);
+            
+            // Fill Txt File With Sorted Numbers
+            //CreateNumbers(stream);
+            
+            // Fill Txt File with Random Numbers
+            CreateRandomNumberFile(stream);
+            
+            // Create a random int List
+            //var randList = CreateRandomNumberList(stream);
+            
+            // Create a sorted List
+            var sortList = new List<int>() {1,2,3,4,5,6,8,10,15,254,8651,50000}; 
+            
+            //Console.WriteLine(isSorted(randList));
+            //sortList.ForEach(Console.WriteLine);
+            //Console.WriteLine(IsSorted(sortList));
+
+            var convertedList = TxtToList(path);
+            //convertedList.ForEach(Console.Write);
+            Console.WriteLine(IsSorted(convertedList));
+            
+            */
+            
+            /*
+            IntList newList = new IntList(25);
+            newList.PrintList();
+            Console.WriteLine(newList.IsSorted());
+            */
+            Console.WriteLine("Enter amount of num:");
+            int numAmount = int.Parse(Console.ReadLine());
+            
+            
+            IntList newBubble = new IntList(numAmount);
+            Console.WriteLine("This is the new unsorted List:");
+            
+            //newBubble.PrintList();
+            
+            
+            Console.WriteLine("\r\nIs unsorted Bubble Sorted?");
+            Console.WriteLine(newBubble.IsSorted());
+
+            Stopwatch sw1 = new Stopwatch();
+            sw1.Start();
+            List<int> sortedBubble = newBubble.BubbleSort();
+            sw1.Stop();
+            Console.WriteLine("\r\nIs Sorted Bubble Sorted?");
+            Console.WriteLine(IsSorted(sortedBubble));
+            Console.WriteLine("This is the New Sorted List:");
+            
+            //PrintList(sortedBubble);
+            Console.WriteLine($"\r\nTime Elapsed: {sw1.Elapsed}");
+ 
+            
+            
+
         }
+
+
+        public static string AssignPath()
+        {
+            var basepath = "E:\\Documents\\SolutionFiles\\AlgoSort\\";
+            Console.WriteLine("Enter a file name:");
+            var filename = Console.ReadLine() + ".txt";
+            var path = basepath + filename;
+            return path;
+        }
+        
+        
+
+        public static StreamWriter CreateFile(string path)
+        {
+            
+
+            //var stream = File.Create(path);
+            var stream = new StreamWriter(path, true){AutoFlush = true};
+            return stream;
+
+        }
+
+        public static void CreateNumbers(StreamWriter stream)
+        {
+            Console.WriteLine("Enter amount of numbers");
+            var amount = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i <= amount; i++)
+            {
+                stream.WriteLine(i);
+            }
+            stream.Close();
+
+        }
+
+        // Creates a File Filled with random int
+        public static void CreateRandomNumberFile(StreamWriter stream)
+        {
+
+            var numberlist = CreateRandomNumberList(stream);
+
+            foreach (var number in numberlist)
+            {
+                stream.WriteLine(number);
+            }
+            
+            stream.Close();
+        }
+
+
+        // Creates a list filled with randomized int 
+        public static List<int> CreateRandomNumberList(StreamWriter stream)
+        {
+            var numberlist = new List<int>();
+            
+            Console.WriteLine("Enter amount of numbers");
+            var amount = int.Parse(Console.ReadLine());
+
+            // Fill List
+            for (var i = 0; i < amount; i++)
+            {
+                numberlist.Add(i);
+            }
+            
+            // Randomize List using Fisher-Yates Shuffle
+            var rand = new Random();
+            var n = numberlist.Count;
+
+            for (var i = numberlist.Count - 1; i > 1 ; i--)
+            {
+                var random = rand.Next(i + 1);
+                var value = numberlist[random];
+                numberlist[random] = numberlist[i];
+                numberlist[i] = value;
+                
+            }
+            return numberlist;
+        }
+
+        
+        // DEPRECATED
+        public static bool IsSorted(List<int> randList)
+        {
+
+            for (var i = 0; i < randList.Count-1; i++)
+            {
+                if (randList[i] >= randList[i + 1])
+                {
+                    return false;
+                } 
+            }
+            
+            return true;
+            
+        }
+
+
+        public static List<int> TxtToList(string path)
+        {
+            string line;
+            List<int> numList = new List<int>();
+            StreamReader file = new StreamReader(path);
+
+            while ((line = file.ReadLine()) != null)
+            {
+                numList.Add(int.Parse(line));
+            }
+
+            return numList;
+
+        }
+        
+        public static void PrintList(List<int> list)
+        {
+            foreach (var num in list )
+            {
+                Console.Write(num);
+                Console.Write("|");
+            }
+        }
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
